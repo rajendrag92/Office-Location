57b72c87b54782c2266eff2e4bfbf60020a2b773
Raj <rajendra.gupta.onair@gmail.com>
2020-02-23 20:51:25 -0600

    Added Login Form

--- OfficeProject/office-location/src/Features/Login/Login.js
+++ OfficeProject/office-location/src/Features/Login/Login.js
@@ -1,7 +1,53 @@
 import React from 'react'
+import { Row, Col, Form, Button } from 'react-bootstrap'
 
-export default function Login(){
+export default function Login() {
     return (
-        <h1>Login</h1>
+        <Row>
+            <Col>
+                <Row className='mb-5'> 
+                    <Col>
+                        <h2>Login</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form>
+                            <Form.Group controlId="userNameGroup">
+                                <Row>
+                                    <Col md={2}>
+                                        <Form.Label>User</Form.Label>
+                                    </Col>
+                                    <Col md={10}>
+                                        <Form.Control type='text' placeholder="User Name"></Form.Control>
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                            <Form.Group controlId="userPasswordGroup">
+                                <Row>
+                                    <Col md={2}>
+                                        <Form.Label>Password</Form.Label>
+                                    </Col>
+                                    <Col md={10}>
+                                        <Form.Control type='password'></Form.Control>
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+
+                            <Form.Group controlId="userNameGroup">
+                                <hr></hr>
+                                <Row className="mt-3">
+                                    <Col md={{ span: 2, offset: 2 }}>
+
+                                        <Button variant='primary' size='lg'>Submit</Button>
+                                    </Col>
+                                </Row>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
+
+            </Col>
+        </Row>
     )
 }
--- OfficeProject/office-location/src/Features/PageLayout.js
+++ OfficeProject/office-location/src/Features/PageLayout.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 
 
-export default function PageLayout({ component:Component, ...rest }) {
+export default function PageLayout({ component: Component, ...rest }) {
 
     return (
 
         <Route {...rest} render={matchProps => (
-            <div className="DefaultLayout">
-                <div className="Header">Header</div>
-                <Component {...matchProps} />
-                <div className="Footer">Footer</div>
-            </div>
+            <Row className="mt-5">
+                <Col md={{ span: 8, offset: 2 }}>
+                    <Component {...matchProps} />
+                </Col>
+            </Row>
         )} />
     )
 }
